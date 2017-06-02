151fee63a413362f933308f918b9415904edf7ef
carreraa8 <carreraa8@gmail.com>
2017-06-02 14:24:15 -0400

    dcmqr setup

--- src/com/siim/DicomWebToDicom.java
+++ src/com/siim/DicomWebToDicom.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
+import org.dcm4che2.net.ConfigurationException;
 import org.dcm4che2.tool.dcmqr.DcmQR;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,35 +24,39 @@ public class DicomWebToDicom {
     
 	private static final Logger LOG = LoggerFactory.getLogger(DcmQR.class);
     static private String AE = "DCM4CHEE";
-    static private String IP = "localhost";
+    static private String IP = "192.168.56.101";
 
     public static String doDcmQr(Map<String, String> map){
     	String s = "";
     	
         try {
-        	System.out.println("Pre");
+        	System.out.println("Create DCMQR");
         	DcmQR dcmqr = new DcmQR(AE);
         
-        	System.out.println("PreStart");
-        	dcmqr.start();
-        	
         	dcmqr.setCalling(AE);
-        	dcmqr.setLocalHost(IP);
-        	dcmqr.setLocalPort(11112);
+        	dcmqr.setCalledAET(AE, true);
+        	dcmqr.setRemoteHost(IP);
+        	dcmqr.setRemotePort(11112);
+        	dcmqr.configureTransferCapability(true);
         	
-        	System.out.println("PreOpts");
+        	System.out.println("Apply Options");
         	for (Map.Entry<String, String> entry : map.entrySet()) {
                 String key = entry.getKey();
                 String value = entry.getValue();
                 dcmqr.addMatchingKey(Tag.toTagPath(key), value);
         	}
-            
-        	System.out.println(dcmqr.toString());
-        	        
-        	List<DicomObject> list = dcmqr.query();
-        	s = list.toString();
+        	
+        	dcmqr.start();
+        	System.out.println("started");
+        	dcmqr.open();
+        	System.out.println("opened");
+        	List<DicomObject> result = dcmqr.query();
+        	System.out.println("Complete Query");
+        	
+        	s = result.toString();
+        	System.out.println(s);
 			
-		} catch (IOException | InterruptedException e) {
+		} catch (IOException | InterruptedException | ConfigurationException e) {
 			e.printStackTrace();
 		}
  
